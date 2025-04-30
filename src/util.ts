fee0490f582223e9f237ac3c877b2600b4e03236
Priyanshu <priyanshu.singh@contentstack.com>
2025-04-30 22:12:32 +0530

    refactor: update body handling in buildContentstackRequest to use args directly and log wrapper

--- src/util.ts
+++ src/util.ts
@@ -70,31 +70,32 @@ export function buildContentstackRequest(
 
   let body: any = undefined;
 
-  if (actionMapper.complex) {
-    body = buildPayload(actionMapper.body, args);
-  }
-  else {
+  // if (actionMapper.complex) {
+  //   body = buildPayload(actionMapper.body, args);
+  // }
+  // else {
     const bodyKey = actionMapper.body;
     if (typeof bodyKey === "string") {
       const wrapper = actionMapper.wrapKeyword ?? bodyKey;
+      console.log("Wrapper: ", wrapper);
 
-      const consumed = new Set<string>([
-        ...Object.values(actionMapper.params ?? {}),
-        ...Object.values(actionMapper.queryParams ?? {}),
-      ]);
+      // const consumed = new Set<string>([
+      //   ...Object.values(actionMapper.params ?? {}),
+      //   ...Object.values(actionMapper.queryParams ?? {}),
+      // ]);
 
-      const wrapped: Record<string, any> = {};
-      Object.entries(args).forEach(([k, v]) => {
-        if (!consumed.has(k)) {
-          wrapped[k] = v;
-        }
-      });
+      // const wrapped: Record<string, any> = {};
+      // Object.entries(args).forEach(([k, v]) => {
+      //   if (!consumed.has(k)) {
+      //     wrapped[k] = v;
+      //   }
+      // });
 
-      if (Object.keys(wrapped).length > 0) {
-        body = { [wrapper]: wrapped };
-      }
+      // if (Object.keys(wrapped).length > 0) {
+        body = { [wrapper]: args };
+      // }
     }
-  }
+  // }
 
   return {
     method: actionMapper.method,
